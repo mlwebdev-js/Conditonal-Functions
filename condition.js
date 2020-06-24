0c3060aab0f12f60d481edc00c59476118c3c6b1
mlwebdev-js <34560974+mlwebdev-js@users.noreply.github.com>
2020-06-24 17:08:40 -0400

    Add condition.js file

---
+++ condition.js
@@ -0,0 +1,45 @@
+/*
+ * condition.js
+ * functions to test conditions with callback function
+ * MLL - 06/24/2020
+*/
+
+const condition = {
+  lessThan: function lessThanOrEqual(a, b, function2Call){
+    if( a < b) function2Call();
+  },
+  lessThanOrEqual: function lessThanOrEqual(a, b, function2Call){
+    if( a <= b) function2Call();
+  },
+  greaterThan: function greaterThanOrEqual(a, b, function2Call){
+    if( a > b) function2Call();
+  },
+  greaterThanOrEqual: function greaterThanOrEqual(a, b, function2Call){
+    if( a >= b) function2Call();
+  },
+  isEqual: function isEqual(a, b, function2Call){
+    if( a == b) function2Call();
+  },
+  isTrue: function isTrue(a, function2Call){
+    if(a) function2Call();
+  }, 
+  notEqual: function notEqual(a, b, function2Call){
+    if( a != b) function2Call();
+  },
+  isNot: function isNot(a, function2Call){
+    if(!a) function2Call();
+  }  
+};
+
+ function functionCall(){
+  console.log ("True. a is less than or equal to b");
+}
+
+
+
+
+
+
+
+
+
